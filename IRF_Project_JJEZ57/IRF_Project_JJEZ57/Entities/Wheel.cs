2afdfb5e912b1a3b40ead048c1f0c6accf6a031f
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-12-10 15:49:32 +0100

    Wheel

---
+++ IRF_Project_JJEZ57/IRF_Project_JJEZ57/Entities/Wheel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project_JJEZ57.Entities
+{
+   public class Wheel: Label
+    {
+        public Wheel()
+        {
+            Width = 75;
+            Height = 75;
+            AutoSize = false;
+            Paint += Wheel_Paint;
+        }
+
+        private void Wheel_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+
+        }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+        public void MoveWheel()
+        {
+            Left += 1;
+        }
+    }
+}
---
+++ IRF_Project_JJEZ57/IRF_Project_JJEZ57/Entities/WheelProduction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project_JJEZ57.Entities
+{
+    public class WheelProduction
+    {
+        public Wheel Create()
+        {
+            return new Wheel();
+        }
+    }
+}
