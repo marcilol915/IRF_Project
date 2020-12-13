685d68def1b3a6bade53edb22204911160685b9e
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-12-13 15:54:04 +0100

    Keszletfrissites

--- IRF_Project_JJEZ57/IRF_Project_JJEZ57/Form1.Designer.cs
+++ IRF_Project_JJEZ57/IRF_Project_JJEZ57/Form1.Designer.cs
@@ -30,14 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.buttonMercedes = new System.Windows.Forms.Button();
+            this.buttonBmw = new System.Windows.Forms.Button();
+            this.buttonAudi = new System.Windows.Forms.Button();
+            this.buttonWheel = new System.Windows.Forms.Button();
             this.CreateTimer = new System.Windows.Forms.Timer(this.components);
             this.MoveTimer = new System.Windows.Forms.Timer(this.components);
-            this.buttonWheel = new System.Windows.Forms.Button();
-            this.buttonAudi = new System.Windows.Forms.Button();
-            this.buttonBmw = new System.Windows.Forms.Button();
-            this.buttonMercedes = new System.Windows.Forms.Button();
-            this.buttonColor = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timerWheel = new System.Windows.Forms.Timer(this.components);
+            this.timerBmw = new System.Windows.Forms.Timer(this.components);
+            this.timerAudi = new System.Windows.Forms.Timer(this.components);
+            this.timerMercedes = new System.Windows.Forms.Timer(this.components);
             this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -45,6 +51,7 @@
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Controls.Add(this.dataGridView1);
             this.mainPanel.Controls.Add(this.buttonColor);
             this.mainPanel.Controls.Add(this.buttonMercedes);
             this.mainPanel.Controls.Add(this.buttonBmw);
@@ -54,6 +61,55 @@
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(1098, 617);
             this.mainPanel.TabIndex = 0;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(152, 175);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonColor.TabIndex = 4;
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonMercedes
+            // 
+            this.buttonMercedes.Location = new System.Drawing.Point(30, 303);
+            this.buttonMercedes.Name = "buttonMercedes";
+            this.buttonMercedes.Size = new System.Drawing.Size(75, 23);
+            this.buttonMercedes.TabIndex = 3;
+            this.buttonMercedes.Text = "Mercedes";
+            this.buttonMercedes.UseVisualStyleBackColor = true;
+            this.buttonMercedes.Click += new System.EventHandler(this.buttonMercedes_Click);
+            // 
+            // buttonBmw
+            // 
+            this.buttonBmw.Location = new System.Drawing.Point(30, 263);
+            this.buttonBmw.Name = "buttonBmw";
+            this.buttonBmw.Size = new System.Drawing.Size(75, 23);
+            this.buttonBmw.TabIndex = 2;
+            this.buttonBmw.Text = "BMW";
+            this.buttonBmw.UseVisualStyleBackColor = true;
+            this.buttonBmw.Click += new System.EventHandler(this.buttonBmw_Click);
+            // 
+            // buttonAudi
+            // 
+            this.buttonAudi.Location = new System.Drawing.Point(30, 221);
+            this.buttonAudi.Name = "buttonAudi";
+            this.buttonAudi.Size = new System.Drawing.Size(75, 23);
+            this.buttonAudi.TabIndex = 1;
+            this.buttonAudi.Text = "Audi";
+            this.buttonAudi.UseVisualStyleBackColor = true;
+            this.buttonAudi.Click += new System.EventHandler(this.buttonAudi_Click);
+            // 
+            // buttonWheel
+            // 
+            this.buttonWheel.Location = new System.Drawing.Point(30, 176);
+            this.buttonWheel.Name = "buttonWheel";
+            this.buttonWheel.Size = new System.Drawing.Size(75, 23);
+            this.buttonWheel.TabIndex = 0;
+            this.buttonWheel.Text = "Wheel";
+            this.buttonWheel.UseVisualStyleBackColor = true;
+            this.buttonWheel.Click += new System.EventHandler(this.buttonWheel_Click);
             // 
             // CreateTimer
             // 
@@ -67,54 +123,35 @@
             this.MoveTimer.Interval = 10;
             this.MoveTimer.Tick += new System.EventHandler(this.MoveTimer_Tick);
             // 
-            // buttonWheel
+            // dataGridView1
             // 
-            this.buttonWheel.Location = new System.Drawing.Point(30, 176);
-            this.buttonWheel.Name = "buttonWheel";
-            this.buttonWheel.Size = new System.Drawing.Size(75, 23);
-            this.buttonWheel.TabIndex = 0;
-            this.buttonWheel.Text = "Wheel";
-            this.buttonWheel.UseVisualStyleBackColor = true;
-            this.buttonWheel.Click += new System.EventHandler(this.buttonWheel_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(477, 221);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(428, 150);
+            this.dataGridView1.TabIndex = 5;
             // 
-            // buttonAudi
+            // timerWheel
             // 
-            this.buttonAudi.Location = new System.Drawing.Point(30, 221);
-            this.buttonAudi.Name = "buttonAudi";
-            this.buttonAudi.Size = new System.Drawing.Size(75, 23);
-            this.buttonAudi.TabIndex = 1;
-            this.buttonAudi.Text = "Audi";
-            this.buttonAudi.UseVisualStyleBackColor = true;
-            this.buttonAudi.Click += new System.EventHandler(this.buttonAudi_Click);
+            this.timerWheel.Interval = 3000;
+            this.timerWheel.Tick += new System.EventHandler(this.timerWheel_Tick);
             // 
-            // buttonBmw
+            // timerBmw
             // 
-            this.buttonBmw.Location = new System.Drawing.Point(30, 263);
-            this.buttonBmw.Name = "buttonBmw";
-            this.buttonBmw.Size = new System.Drawing.Size(75, 23);
-            this.buttonBmw.TabIndex = 2;
-            this.buttonBmw.Text = "BMW";
-            this.buttonBmw.UseVisualStyleBackColor = true;
-            this.buttonBmw.Click += new System.EventHandler(this.buttonBmw_Click);
+            this.timerBmw.Interval = 3000;
+            this.timerBmw.Tick += new System.EventHandler(this.timerBmw_Tick);
             // 
-            // buttonMercedes
+            // timerAudi
             // 
-            this.buttonMercedes.Location = new System.Drawing.Point(30, 303);
-            this.buttonMercedes.Name = "buttonMercedes";
-            this.buttonMercedes.Size = new System.Drawing.Size(75, 23);
-            this.buttonMercedes.TabIndex = 3;
-            this.buttonMercedes.Text = "Mercedes";
-            this.buttonMercedes.UseVisualStyleBackColor = true;
-            this.buttonMercedes.Click += new System.EventHandler(this.buttonMercedes_Click);
+            this.timerAudi.Interval = 3000;
+            this.timerAudi.Tick += new System.EventHandler(this.timerAudi_Tick);
             // 
-            // buttonColor
+            // timerMercedes
             // 
-            this.buttonColor.Location = new System.Drawing.Point(152, 175);
-            this.buttonColor.Name = "buttonColor";
-            this.buttonColor.Size = new System.Drawing.Size(75, 23);
-            this.buttonColor.TabIndex = 4;
-            this.buttonColor.UseVisualStyleBackColor = true;
-            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            this.timerMercedes.Interval = 3000;
+            this.timerMercedes.Tick += new System.EventHandler(this.timerMercedes_Tick);
             // 
             // Form1
             // 
@@ -125,6 +162,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.mainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -139,6 +177,11 @@
         private System.Windows.Forms.Button buttonAudi;
         private System.Windows.Forms.Button buttonWheel;
         private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timerWheel;
+        private System.Windows.Forms.Timer timerBmw;
+        private System.Windows.Forms.Timer timerAudi;
+        private System.Windows.Forms.Timer timerMercedes;
     }
 }
 
--- IRF_Project_JJEZ57/IRF_Project_JJEZ57/Form1.cs
+++ IRF_Project_JJEZ57/IRF_Project_JJEZ57/Form1.cs
@@ -14,7 +14,123 @@ namespace IRF_Project_JJEZ57
 {
     public partial class Form1 : Form
     {
+        CarsEntities1 context = new CarsEntities1();
         private List<Toy> toys = new List<Toy>();
+        public void Keszletfrissites()
+        {
+            var keszlet = from x in context.Termek
+                          select new
+                          {
+                              Termék = x.Nev,
+                              Db = x.Darab
+                          };
+            dataGridView1.DataSource = keszlet.ToList();
+
+        }
+        public void WheelAdd()
+        {
+            var db = (from x in context.Termek
+                     where x.Nev == "Kerek"
+                     select x.Darab).FirstOrDefault();
+            var egysar = (from x in context.Termek
+                          where x.Nev == "Kerek"
+                          select x.Egységár).FirstOrDefault();
+            var del = (from x in context.Termek
+                      where x.Nev == "Kerek"
+                      select x).FirstOrDefault();
+            context.Termek.Remove(del);
+            Termek t = new Termek();
+            t.Nev = "Kerek";
+            t.Darab = Convert.ToInt32(db)+1;
+            t.Egységár = Convert.ToInt32(egysar);
+            context.Termek.Add(t);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void BmwAdd()
+        {
+            var db = (from x in context.Termek
+                      where x.Nev == "BMW"
+                      select x.Darab).FirstOrDefault();
+            var egysar = (from x in context.Termek
+                          where x.Nev == "BMW"
+                          select x.Egységár).FirstOrDefault();
+            var del = (from x in context.Termek
+                       where x.Nev == "BMW"
+                       select x).FirstOrDefault();
+            context.Termek.Remove(del);
+            Termek t = new Termek();
+            t.Nev = "BMW";
+            t.Darab = Convert.ToInt32(db) + 1;
+            t.Egységár = Convert.ToInt32(egysar);
+            context.Termek.Add(t);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void AudiAdd()
+        {
+            var db = (from x in context.Termek
+                      where x.Nev == "Audi"
+                      select x.Darab).FirstOrDefault();
+            var egysar = (from x in context.Termek
+                          where x.Nev == "Audi"
+                          select x.Egységár).FirstOrDefault();
+            var del = (from x in context.Termek
+                       where x.Nev == "Audi"
+                       select x).FirstOrDefault();
+            context.Termek.Remove(del);
+            Termek t = new Termek();
+            t.Nev = "Audi";
+            t.Darab = Convert.ToInt32(db) + 1;
+            t.Egységár = Convert.ToInt32(egysar);
+            context.Termek.Add(t);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void MercedesAdd()
+        {
+            var db = (from x in context.Termek
+                      where x.Nev == "Mercedes"
+                      select x.Darab).FirstOrDefault();
+            var egysar = (from x in context.Termek
+                          where x.Nev == "Mercedes"
+                          select x.Egységár).FirstOrDefault();
+            var del = (from x in context.Termek
+                       where x.Nev == "Mercedes"
+                       select x).FirstOrDefault();
+            context.Termek.Remove(del);
+            Termek t = new Termek();
+            t.Nev = "Mercedes";
+            t.Darab = Convert.ToInt32(db) + 1;
+            t.Egységár = Convert.ToInt32(egysar);
+            context.Termek.Add(t);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         private IToyFactory _production;
         public IToyFactory Production
         {
@@ -24,7 +140,9 @@ namespace IRF_Project_JJEZ57
         public Form1()
         {
             InitializeComponent();
-            Production = new AudiProduction();
+            Production =new AudiProduction();
+            Keszletfrissites();
+            timerAudi.Enabled = true;
         }
 
         private void CreateTimer_Tick(object sender, EventArgs e)
@@ -56,21 +174,37 @@ namespace IRF_Project_JJEZ57
         private void buttonWheel_Click(object sender, EventArgs e)
         {
             Production = new WheelProduction();
+            timerWheel.Enabled = true;
+            timerMercedes.Enabled = false;
+            timerBmw.Enabled = false;
+            timerAudi.Enabled = false;
         }
 
         private void buttonAudi_Click(object sender, EventArgs e)
         {
             Production = new AudiProduction();
+            timerWheel.Enabled = false;
+            timerMercedes.Enabled = false;
+            timerBmw.Enabled = false;
+            timerAudi.Enabled = true;
         }
 
         private void buttonBmw_Click(object sender, EventArgs e)
         {
             Production = new BmwProduction();
+            timerWheel.Enabled = false;
+            timerMercedes.Enabled = false;
+            timerBmw.Enabled = true;
+            timerAudi.Enabled = false;
         }
 
         private void buttonMercedes_Click(object sender, EventArgs e)
         {
             Production = new MercedesProduction();
+            timerWheel.Enabled = false;
+            timerMercedes.Enabled = true;
+            timerBmw.Enabled = false;
+            timerAudi.Enabled = false;
         }
 
         private void buttonColor_Click(object sender, EventArgs e)
@@ -83,6 +217,30 @@ namespace IRF_Project_JJEZ57
             button.BackColor = colorPicker.Color;
             Production = new WheelProduction
             { WheelColor=buttonColor.BackColor};
+        }
+
+        private void timerWheel_Tick(object sender, EventArgs e)
+        {
+            WheelAdd();
+            Keszletfrissites();
+        }
+
+        private void timerBmw_Tick(object sender, EventArgs e)
+        {
+            BmwAdd();
+            Keszletfrissites();
+        }
+
+        private void timerAudi_Tick(object sender, EventArgs e)
+        {
+            AudiAdd();
+            Keszletfrissites();
+        }
+
+        private void timerMercedes_Tick(object sender, EventArgs e)
+        {
+            MercedesAdd();
+            Keszletfrissites();
         }
     }
 }
